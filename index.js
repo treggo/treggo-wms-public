cab6f32510cb4dbd3bb0fe950856cd47f71f87ea
Nicolas Torchio <nicolas.a.torchio@gmail.com>
2022-03-24 14:27:50 -0300

    Using blessed to update UI to TUI

--- index.js
+++ index.js
@@ -1,18 +1,57 @@
-const readline = require('readline');
+const blessed = require('blessed')
 const axios = require('axios');
 const args = require("args-parser")(process.argv)
 
-const rl = readline.createInterface({
-  input: process.stdin,
+const screen = blessed.screen({
+    smartCSR: true
+});
+screen.key(['q', 'C-c', 'escape'], function quit() { return process.exit(0); });
+
+const table = blessed.table({ top: 3, border: 'line', width: "30%" });
+const log = blessed.log({ top: 3, border: 'line', left: "30%" });
+const input = blessed.textbox({ height: 3, border: 'line', inputOnFocus: true });
+
+let q = {
+    scans: 0,
+    oks: 0,
+    errors: 0,
+}
+
+input.key(['enter'], () => input.submit());
+input.on('submit', () => {
+    let text = input.getValue();
+    input.setValue("")
+    input.focus();
+    q.scans++;
+    updateStatus()
+    axios.post("https://" + args.url + "/0/point/checkin", { pistol: text, scanner: args.scanner }, { headers: { Authorization: "Bearer " + args.secret } })
+        .then((response) => {
+            q.oks++;
+            updateStatus()
+        })
+        .catch((err) => {
+            log.log(err.toString())
+            q.errors++;
+            updateStatus()
+        })
+        .finally(() => {
+            updateStatus()
+        })
 });
 
-rl.question("",newScan);
-function newScan(pistol){
-    rl.question("",newScan);
-    axios.post("https://api.ar.treggo.co/0/point/checkin",{pistol,scanner:args.scanner},{headers:{Authorization:"Bearer " + args.secret}})
-    .then((response) => {
-      console.log("scan",cantidad,": ",response.data.message)
-    }).catch((err) => {
-      console.log(err.toString())
-    })
+screen.append(log);
+screen.append(table);
+screen.append(input);
+input.focus();
+updateStatus()
+
+screen.render();
+
+function updateStatus() {
+    table.setData([
+        ['Escaneos', `${q.scans}`],
+        ['Correctos', `${q.oks}`],
+        ['Erroneos', `${q.errors}`]
+    ]);
+    screen.render();
 }
